18673207db26c0793400c8653689c13805b2120b
jopetty <research@jacksonpetty.org>
2024-12-19 16:23:32 -0700

    finish cleaning script

--- rcourse_lesson5/scripts/cleaning.R
+++ rcourse_lesson5/scripts/cleaning.R
@@ -16,3 +16,24 @@ data_states = read.table("data/rcourse_lesson5_data_states.txt", header=T, sep="
 
 # See how many states in union versus confederacy
 xtabs(~civil_war, data_states)
+
+## CLEAN DATA ####
+# Make data set balanced for Union and Confederacy states
+data_states_clean = data_states %>%
+  filter(!is.na(civil_war)) %>%
+  group_by(civil_war) %>%
+  arrange(order_enter) %>%
+  filter(row_number() <= 11) %>%
+  ungroup()
+
+# Double check balanced for 'civil_war' variable
+xtabs(~civil_war, data_states_clean)
+
+# Combine three data frames
+data_clean = data_election_results %>%
+  inner_join(data_elections) %>%
+  inner_join(data_states_clean) %>%
+  mutate(state = factor(state))
+
+# Double check all of numbers are balanced
+xtabs(~incumbent_party+civil_war, data_clean)
