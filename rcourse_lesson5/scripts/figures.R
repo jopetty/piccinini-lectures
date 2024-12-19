8e6e737443c98b845be4189864bd0ae48f615c5f
jopetty <research@jacksonpetty.org>
2024-12-19 16:26:27 -0700

    figures
    '

--- rcourse_lesson5/scripts/figures.R
+++ rcourse_lesson5/scripts/figures.R
@@ -3,3 +3,24 @@ source("scripts/cleaning.R")
 
 ## LOAD PACKAGES ####
 library(ggplot2)
+
+## ORGANIZE DATA ####
+data_figs = data_clean %>%
+  mutate(civil_war = factor(civil_war,
+                            levels = c("union", "confederacy"),
+                            labels = c("Union", "Confederacy"))) %>%
+  mutate(incumbent_party = factor(incumbent_party,
+                                  levels = c("democrat", "republican"),
+                                  labels = c("Democrat", "Republican")))
+
+## MAKE FIGURES ####
+# Histogram of full data set
+incumbent_histogram_full.plot = ggplot(data_figs, aes(x = perc_votes_incumbent,
+                                                      fill = incumbent_party)) +
+  geom_histogram(bins = 10) +
+  facet_grid(incumbent_party ~ civil_war) +
+  scale_fill_manual(values = c("blue", "red"))
+
+pdf("figures/incumbent_histogram_full.pdf")
+incumbent_histogram_full.plot
+dev.off()
