043105ca129d41bff70740f48c8fa74f164b4545
jopetty <research@jacksonpetty.org>
2024-12-20 09:42:50 -0700

    finish stats2

--- rcourse_lesson6/scripts/stats2.R
+++ rcourse_lesson6/scripts/stats2.R
@@ -3,3 +3,79 @@ source("scripts/cleaning.R")
 
 ## LOAD PACKAGES ####
 library(lme4)
+
+## ORGANIZE DATA ####
+# Accuracy data
+data_accuracy_stats = data_accuracy_clean %>%
+  mutate(congruency_contrast = ifelse(congruency == "con", -0.5, 0.5)) %>%
+  mutate(half_contrast = ifelse(half == "first", -0.5, 0.5))
+
+xtabs(~congruency+congruency_contrast, data_accuracy_stats)
+xtabs(~half+half_contrast, data_accuracy_stats)
+
+# RT data
+data_rt_stats = data_rt_clean %>%
+  mutate(congruency_contrast = ifelse(congruency == "con", -0.5, 0.5)) %>%
+  mutate(half_contrast = ifelse(half == "first", -0.5, 0.5))
+
+xtabs(~congruency+congruency_contrast, data_rt_stats)
+xtabs(~half+half_contrast, data_rt_stats)
+
+## BUILD MODELS FOR ACCURACY ANALYSIS ####
+# Full model
+# accuracy.glmer = glmer(accuracy ~ congruency_contrast * half_contrast +
+#                          (1+congruency_contrast*half_contrast|subject_id) +
+#                          (1+half_contrast|item), family = "binomial",
+#                        data = data_accuracy_stats)
+
+accuracy.glmer = glmer(accuracy ~ congruency_contrast * half_contrast +
+                         (1|subject_id) +
+                         (0+half_contrast|subject_id) +
+                         (1|item), family = "binomial",
+                       data = data_accuracy_stats)
+
+# Summarise model and save
+accuracy.glmer_sum = summary(accuracy.glmer)
+accuracy.glmer_sum
+
+# Test for effect of congruency
+accuracy_congruency.glmer = glmer(accuracy ~ congruency_contrast * half_contrast -
+                                    congruency_contrast +
+                                    (1|subject_id) +
+                                    (0+half_contrast|subject_id) +
+                                    (1|item), family = "binomial",
+                                  data = data_accuracy_stats)
+
+accuracy_congruency.anova = anova(accuracy.glmer, accuracy_congruency.glmer)
+accuracy_congruency.anova
+
+# Test for effect of experiment half
+accuracy_half.glmer = glmer(accuracy ~ congruency_contrast * half_contrast -
+                              half_contrast +
+                              (1|subject_id) +
+                              (0+half_contrast|subject_id) +
+                              (1|item), family = "binomial",
+                            data = data_accuracy_stats)
+
+accuracy_half.anova = anova(accuracy.glmer, accuracy_half.glmer)
+accuracy_half.anova
+
+# Test for interaction of congruency x experiment half
+accuracy_congruencyxhalf.glmer = glmer(accuracy ~ congruency_contrast * half_contrast -
+                                         congruency_contrast:half_contrast +
+                                         (1|subject_id) +
+                                         (0+half_contrast|subject_id) +
+                                         (1|item), family = "binomial",
+                                       data = data_accuracy_stats)
+
+accuracy_congruencyxhalf.anova = anova(accuracy.glmer, accuracy_congruencyxhalf.glmer)
+accuracy_congruencyxhalf.anova
+
+
+## BUILD MODELS FOR REACTION TIME ANALYSIS ####
+# Full model
+rt_log10.lmer = lmer(rt_log10 ~ congruency_contrast * half_contrast +
+                       (1+congruency_contrast*half_contrast|subject_id) +
+                       (1|item) +
+                       (0+half_contrast|item), REML = F,
+                     data = data_rt_stats)
